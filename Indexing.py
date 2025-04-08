006d4ce6ff5aeea3a524db4661044492bb378552
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2025-04-08 15:07:43 +0530

    Create Indexing.py

---
+++ Indexing.py
@@ -0,0 +1,14 @@
+#Modifying element of an array using indexing
+fruits = ["Apple","Banana","Mango","Grapes","orange"]
+print(fruits)
+
+fruits[1] = "pineapple"
+print(fruits)
+
+fruits[-1] = "Guava"
+print(fruits)
+
+#output #
+#['Apple', 'Banana', 'Mango', 'Grapes', 'orange']
+#['Apple', 'pineapple', 'Mango', 'Grapes', 'orange']
+#['Apple', 'pineapple', 'Mango', 'Grapes', 'Guava']
