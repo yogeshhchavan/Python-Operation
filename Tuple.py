599abfea97635108393515e169b0980e17978c47
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2025-04-09 15:24:44 +0530

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,16 @@
+#Creating Empty Tuple
+tuple1 = ()
+print(tuple1)
+
+#creating tuple with integer elements
+tuple2 = (1,2,3)
+print(tuple2)
+
+#creating tuple with mixed datatyped
+tuple3 = (101, "Anirban", 20000.00, "HR dept")
+print(tuple3)
+
+#output
+()
+(1, 2, 3)
+(101, 'Anirban', 20000.0, 'HR dept')
