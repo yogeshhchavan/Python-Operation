888ec347df4161efe98361401e9b6981627bb77e
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2025-05-08 15:33:38 +0530

    Create while continue.py

---
+++ while continue.py
@@ -0,0 +1,16 @@
+#print 1 to 10 odd number or neglate even number in between
+i = 0
+while i <= 10:
+    if(i %2 == 0):
+        i += 1
+        continue
+    print(i)
+    i += 1
+  
+  
+  #Output#
+  1
+  3
+  5
+  7
+  9
