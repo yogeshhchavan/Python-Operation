4b1f07a1b6f403fbd577ef221d3b9eb160587a18
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2025-05-08 15:35:24 +0530

    Update while continue.py

--- while continue.py
+++ while continue.py
@@ -14,3 +14,21 @@ while i <= 10:
   5
   7
   9
+
+
+#print 1 to 10 even number or neglate Odd number in between
+i = 0
+while i <= 10:
+    if(i %2 != 0):
+        i += 1
+        continue
+    print(i)
+    i += 1
+
+#Output#
+0
+2
+4
+6
+8
+10
