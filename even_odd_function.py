7bf43304d456d392c743d06d3c62d90a386f2602
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2025-05-28 10:49:48 +0530

    Create even_odd_function.py

---
+++ even_odd_function.py
@@ -0,0 +1,8 @@
+#WAP to print even odd number using function, take input from the user.
+def number(n):
+    if n % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+number(int(input("Enter the number")))
+#output : Odd
